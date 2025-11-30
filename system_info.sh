d33f128d97ea383cdacbca0c723a4e88cb2a60d5
NJEI PIERRICK Jnr <njei.private@gmail.com>
2025-11-30 14:05:50 +0100

    Add memory and disk space information to script

--- system_info.sh
+++ system_info.sh
@@ -6,5 +6,7 @@ echo "OS: $(uname -s)"
 echo "Release: $(uname -r)"
 echo "Architecture: $(uname -m)" 
 echo "CPU: $(lscpu | grep 'Model name' | cut -d':' -f2-)"
+echo "Memory: $(free -h | grep 'Mem' | awk '{print $2}')"
+echo "Disk Space: $(df -h | grep '^/dev' | awk '{print $1, $5, $6}' )"
 
 
