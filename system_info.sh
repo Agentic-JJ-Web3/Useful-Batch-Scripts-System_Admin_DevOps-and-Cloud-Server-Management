888fcbddd0b28ee5133efccbecea122da6509098
NJEI PIERRICK Jnr <njei.private@gmail.com>
2025-11-30 14:02:14 +0100

    Add CPU information to system_info.sh

--- system_info.sh
+++ system_info.sh
@@ -4,5 +4,7 @@ echo "------------------"
 echo "Hostname: $(hostname)"
 echo "OS: $(uname -s)"
 echo "Release: $(uname -r)"
-echo "Architecture: $(uname -m)"
+echo "Architecture: $(uname -m)" 
+echo "CPU: $(lscpu | grep 'Model name' | cut -d':' -f2-)"
+
 
