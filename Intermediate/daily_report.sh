4e28b4f3f62d131519c99d4389492767ad1e4667
Agentic-JJ-Web3 <njei.private@gmail.com>
2025-12-08 13:13:18 +0100

    Adding data to the  REPORT_FILE

--- Intermediate/daily_report.sh
+++ Intermediate/daily_report.sh
@@ -3,3 +3,8 @@
 #  Gather disk usage, failed, failed logins, and active processes
 
 REPORT_FILE="/tmp/daily_report.txt"
+
+# Start the report
+echo "==== Daily Ssystem Report ====" > "$REPORT_FILE"
+echo "Date: $(date)" >> "$REPORT_FILE"
+echo >> "$REPORT_FILE"
